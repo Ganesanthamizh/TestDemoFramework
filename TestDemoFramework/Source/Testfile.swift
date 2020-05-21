ee54a765300ef25ea1d298ff82a98d27da55a7bd
Ganesanthamizh <erganeshtamizh@gmail.com>
2020-05-21 20:05:45 +0530

    Added Test cases

---
+++ TestDemoFramework/Source/Testfile.swift
@@ -0,0 +1,21 @@
+//
+//  Testfile.swift
+//  TestDemoFramework
+//
+//  Created by user on 21/05/20.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import Foundation
+
+public final class TestFile {
+    let name = "Testfile"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
--- TestDemoFrameworkTests/TestDemoFrameworkTests.swift
+++ TestDemoFrameworkTests/TestDemoFrameworkTests.swift
@@ -10,13 +10,23 @@ import XCTest
 @testable import TestDemoFramework
 
 class TestDemoFrameworkTests: XCTestCase {
+    
+    var testFile:TestFile!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        testFile = TestFile()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    func testAdd() {
+        XCTAssertEqual(testFile.add(a: 1, b: 1), 2)
+    }
+    
+    func testSub() {
+        XCTAssertEqual(testFile.sub(a: 2, b: 1), 1)
     }
 
     func testExample() throws {
