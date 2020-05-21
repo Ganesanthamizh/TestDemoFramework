a929eb05276d9ba09135d9cb091ecae0a077515e
Ganesanthamizh <erganeshtamizh@gmail.com>
2020-05-21 18:28:50 +0530

    first commit

---
+++ TestDemoFramework/TestDemoFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestDemoFramework.h
+//  TestDemoFramework
+//
+//  Created by user on 21/05/20.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestDemoFramework.
+FOUNDATION_EXPORT double TestDemoFrameworkVersionNumber;
+
+//! Project version string for TestDemoFramework.
+FOUNDATION_EXPORT const unsigned char TestDemoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestDemoFramework/PublicHeader.h>
+
+
